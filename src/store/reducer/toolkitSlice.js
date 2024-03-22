90a6440c360e8f3499ab1c0901bb6d1e1b2923d8
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-03-23 00:13:42 +0500

    Update toolkitSlice.js

--- src/store/reducer/toolkitSlice.js
+++ src/store/reducer/toolkitSlice.js
@@ -7,20 +7,9 @@ const toolkitSlice = createSlice({
         todos: ['nikita', 'oleg', 'sonya']
     },
     reducers: {
-        increment(state) {
-            state.count += 1;
-        },
-        decrement(state) {
-            state.count -= 1;
-        },
-        addTodos(state, action) {
-            state.todos.push(action)
-        },
-        removeLastTodo(state) {
-            state.todos.pop()
-        }
+        
     }
 })
 
 export default toolkitSlice.reducer
-export const {increment, decrement, addTodos, removeLastTodo} = toolkitSlice.actions
+export const { } = toolkitSlice.actions
