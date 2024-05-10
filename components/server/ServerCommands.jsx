461c956acba8617120a2b593c25833cf0daa6c28
YarnyanDev <92979595+Yarnyan@users.noreply.github.com>
2024-05-10 21:17:11 +0500

    Delete components directory

--- components/server/ServerCommands.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './ServerComponent.module.scss'
-export default function ServerCommands() {
-  return (
-    <div className={styles.ServerCommands}>
-        <p>/afk - включить режим «Нет на месте»</p>
-    </div>
-  )
-}
